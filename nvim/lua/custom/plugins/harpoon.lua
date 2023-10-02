edc2b1d76ee1fbfca04f9bf52df455fa28c24d9a
Nathan Harmer <nathan.harmer@radlink.com.au>
2023-10-02 08:49:55 +0800

    changed the size of harpoon and telescope menu

--- nvim/lua/custom/plugins/harpoon.lua
+++ nvim/lua/custom/plugins/harpoon.lua
@@ -4,6 +4,12 @@ return {
   config = function()
     local mark = require('harpoon.mark')
     local ui = require('harpoon.ui')
+	require('harpoon').setup({
+		menu = {
+			width = vim.api.nvim_win_get_width(0) - 4,
+			height = vim.api.nvim_win_get_height(0) - 4
+		}
+	})
 
     vim.keymap.set("n","<leader>m", mark.add_file, {desc = "Mark File with harpoon"})
     vim.keymap.set("n","<leader>hm", ui.toggle_quick_menu, {desc = "Harpoon Quick Menu"})
