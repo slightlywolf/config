24dfe0e2bbbf7ebbfe4108d8f9bfdad9ae9b0fcd
Nathan Harmer <nathan.harmer@radlink.com.au>
2024-03-25 14:50:44 +0800

    Added markdown preview

--- nvim/lua/custom/plugins/markdownpreview.lua
+++ nvim/lua/custom/plugins/markdownpreview.lua
@@ -1,13 +1,24 @@
 return
 {
-    'toppair/peek.nvim',
-    opts =
-    {
-        run = 'deno task --quiet build:fast'
-    },
-    config = function()
-        vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
-        vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
-    end,
+	{
+		'toppair/peek.nvim',
+		opts =
+		{
+			run = 'deno task --quiet build:fast'
+		},
+		config = function()
+			vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
+			vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
+		end,
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
 
 }
