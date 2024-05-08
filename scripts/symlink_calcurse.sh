130d9690b78a2c2c7e1b3ac59fed2f4f449162fb
slightlywolf <nathanharmer0@gmail.com>
2024-05-08 10:46:26 +0800

    removed the secret smh

--- scripts/symlink_calcurse.sh
+++ scripts/symlink_calcurse.sh
@@ -4,5 +4,8 @@ set -x
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 BASEDIR=$SCRIPTPATH/..
 
+mv ~/.config/calcurse ~/.config/calcurse_backup || true
+
 unlink ~/.calcurse || true
-ln -s $BASEDIR/calcurse ~/.calcurse 
+ln -s $BASEDIR/calcurse ~/.config/calcurse
+
