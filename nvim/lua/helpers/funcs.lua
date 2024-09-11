99083fb2775d9b7165fa2aaa117839c2567acba8
Nathan Harmer <nathan.harmer@radlink.com.au>
2024-09-11 08:04:59 +0800

    changed some themes

--- nvim/lua/helpers/funcs.lua
+++ nvim/lua/helpers/funcs.lua
@@ -1,60 +1,11 @@
--- blink-light
--- Base2Tone-Morning-light
--- for some reason the backgrounds behind tabs become a different color
--- with some schemes, the Base2Tone schemes do not have this issue though
-
 vim.o.background = 'dark'
 vim.o.termguicolors = true
 
-local scheme_whitelist = {
-	--"fox",
-	"Base2Tone",
-	"contrast",
-	"Contrast",
-	-- "light",
-	-- "monokai-soda",
-	-- "duskfox",
-
-}
-
-local scheme_blacklist = {
-	-- light themes specifically
-	
-	-- "laederon",
-	-- "nicotine",
-	-- "pleasant",
-	--
-	--
-	"light",
-	"Light",
-	-- "flight",
-	
-	-- other themes
-	-- "Base2Tone-Lake-Dark",
-	-- "Base2Tone-Sea-Dark",
-	-- "Base2Tone-Meadow-Dark",
-	-- "Base2Tone-Lake-dark",
-	-- "Base2Tone-Sea-dark",
-	-- "Base2Tone-Meadow-dark",
-	-- "Base2Tone-Lakedark",
-	-- "Base2Tone-Seadark",
-	-- "Base2Tone-Meadowdark",
-	-- "Base2Tone-LakeDark",
-	-- "Base2Tone-SeaDark",
-	-- "Base2Tone-MeadowDark",
-	-- "bold-light",
-	-- "contrasty",
-}
-
+local scheme_blacklist = { }
 
 local other_color_filters = {
-	function(colorscheme_name) -- contrast
-		if string.find(colorscheme_name, "contrast") then
-			return true
-		end
-
-		return false
-	end,
+	ColorIsBlackListed,
+	NoDefaultTheme,
 }
 
 function C_filter_copy_array(arr, func)
@@ -84,7 +35,7 @@ function C_get_random_colorscheme(filter_func)
 	local rand_scheme = colors_to_use[math.ceil(#colors_to_use * math.random())]
 
 	local tries = 0
-	while (rand_scheme == vim.g.colors_name and #colors_to_use > 1 and tries < 10)  do
+	while (rand_scheme == vim.g.colors_name and #colors_to_use > 1 and tries < 10) do
 		--
 		-- if there is more than one color than
 
@@ -108,8 +59,9 @@ function C_get_random_colorscheme_list(filter_func)
 	local func_to_use = filter_func or C_all_filter
 
 	local my_colors = vim.fn.getcompletion("", "color")
-	-- local colors_to_use = C_filter_copy_array(my_colors, func_to_use)
-	local colors_to_use = my_colors
+	local colors_to_use = C_filter_copy_array(my_colors, func_to_use)
+	-- uncomment this to use all
+	--local colors_to_use = my_colors
 
 	return colors_to_use
 end
@@ -127,10 +79,10 @@ function C_set_random_colorscheme(filter_func)
 	local color = C_get_random_colorscheme(filter_func)
 
 	if pcall(
-			function()
-				vim.cmd.colorscheme(color)
-			end
-		) then
+		    function()
+			    vim.cmd.colorscheme(color)
+		    end
+	    ) then
 		-- do nothing
 	else
 		SetRandomColorScheme(filter_func)
@@ -139,30 +91,20 @@ end
 
 -- combines a large number of filters into one
 function C_all_filter(colorscheme_name)
-	--whitelist acts before other filters
-	if ColorIsBlackListed(colorscheme_name) then
-		return false
-	end
-
-	if ColorIsWhitelisted(colorscheme_name) then
-		return true
-	end
-
-
 	for _, colorfilter in ipairs(other_color_filters) do
 		if not colorfilter(colorscheme_name) then
 			return false
 		end
 	end
-
-	-- change this to true for all schemes but those blacklisted
-	-- otherwise false to only have the whitelisted schemes
-	local AllExceptBlackListed = true
-	local OnlyWhitelisted = false
-	return AllExceptBlackListed
+	return true
 end
 
-function ColorIsBlackListed(colorscheme_name)
+-- ------------------------------------------------------------------------------------
+-- Filters
+-- ------------------------------------------------------------------------------------
+
+-- This is a partial black list
+function ColorIsBlackListedPartial(colorscheme_name)
 	for _, v in ipairs(scheme_blacklist) do
 		if string.find(colorscheme_name, v) or colorscheme_name == v then
 			return true
@@ -172,12 +114,52 @@ function ColorIsBlackListed(colorscheme_name)
 	return false
 end
 
-function ColorIsWhitelisted(colorscheme_name)
-	for _, v in ipairs(scheme_whitelist) do
-		if string.find(colorscheme_name, v) or colorscheme_name == v then
-			return true
+function ColorIsBlackListed(colorscheme_name)
+	for _, v in ipairs(scheme_blacklist) do
+		if colorscheme_name == v then
+			return false
 		end
 	end
 
-	return false
+	return true
 end
+
+local default_theme_list = {
+	"blue",
+	"darkblue",
+	"default",
+	"delek",
+	"desert",
+	"elflord",
+	"evening",
+	"habamax",
+	"industry",
+	"koehler",
+	"lunaperche",
+	"morning",
+	"murphy",
+	"pablo",
+	"peachpuff",
+	"quiet",
+	"retrobox",
+	"ron",
+	"shine",
+	"slate",
+	"sorbet",
+	"torte",
+	"vim",
+	"wildcharm",
+	"zaibatsu",
+	"zellner"
+}
+
+function NoDefaultTheme(colorscheme_name)
+	for _, v in ipairs(default_theme_list) do
+		if colorscheme_name == v then
+			return false
+		end
+	end
+
+	return true
+end
+
